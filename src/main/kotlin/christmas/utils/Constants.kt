6ec44a3a883c2f748445c1baabbf0f79c42c63ab
3uomlkh <cw1205@duksung.ac.kr>
2023-11-11 20:49:33 +0900

    feat: 입출력클래스 구현 및 상수 처리

---
+++ src/main/kotlin/christmas/utils/Constants.kt
@@ -0,0 +1,30 @@
+package christmas.utils
+
+object Constants {
+    const val YEAR = 2_023
+    const val WEEK = 7
+    const val CHRISTMAS = 25
+    const val MONTH_TWELVE = 12
+    const val THOUSAND = 1_000
+    const val HUNDRED_TWENTY_THOUSAND = 120_000
+    const val CHAMPAGNE_PRICE = 25_000
+
+    const val HYPHEN = "-"
+    const val COMMA = ","
+
+    const val DATE_MESSAGE = "안녕하세요! 우테코 식당 ${MONTH_TWELVE}월 이벤트 플래너입니다.\n" +
+            "${MONTH_TWELVE}월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
+    const val ORDER_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)"
+    const val START_MESSAGE = "${MONTH_TWELVE}월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"
+    const val ORDER_MENU_MESSAGE = "<주문 메뉴>"
+    const val PRE_DISCOUNT_TOTAL_MESSAGE = "<할인 전 총주문 금액>"
+    const val GIFT_MENU_MESSAGE = "<증정 메뉴>"
+    const val BENEFIT_DETAILS_MESSAGE = "<혜택 내역>"
+    const val BENEFIT_AMOUNT_TOTAL_MESSAGE = "<총혜택 금액>"
+    const val AFTER_DISCOUNT_TOTAL_MESSAGE = "<할인 후 예상 결제 금액>"
+    const val DECEMBER_EVENT_BADGE = "<${MONTH_TWELVE}월 이벤트 배지>"
+    const val EVENT_BADGE_STAR = "별"
+    const val EVENT_BADGE_TREE = "트리"
+    const val EVENT_BADGE_SANTA = "산타"
+    const val NO_EVENT = "없음"
+}
---
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -0,0 +1,54 @@
+package christmas.view
+
+import christmas.utils.Constants.AFTER_DISCOUNT_TOTAL_MESSAGE
+import christmas.utils.Constants.BENEFIT_AMOUNT_TOTAL_MESSAGE
+import christmas.utils.Constants.BENEFIT_DETAILS_MESSAGE
+import christmas.utils.Constants.DATE_MESSAGE
+import christmas.utils.Constants.DECEMBER_EVENT_BADGE
+import christmas.utils.Constants.GIFT_MENU_MESSAGE
+import christmas.utils.Constants.ORDER_MENU_MESSAGE
+import christmas.utils.Constants.ORDER_MESSAGE
+import christmas.utils.Constants.PRE_DISCOUNT_TOTAL_MESSAGE
+import christmas.utils.Constants.START_MESSAGE
+
+class OutputView {
+    fun printAskDate() {
+        println(DATE_MESSAGE)
+    }
+
+    fun printAskMenu() {
+        println(ORDER_MESSAGE)
+    }
+
+    fun printStartMessage() {
+        println(START_MESSAGE)
+    }
+
+    fun printMenu() {
+        println(ORDER_MENU_MESSAGE)
+    }
+
+    fun printPreDiscountTotal() {
+        println(PRE_DISCOUNT_TOTAL_MESSAGE)
+    }
+
+    fun printGiftMenu() {
+        println(GIFT_MENU_MESSAGE)
+    }
+
+    fun printBenefitDetails() {
+        println(BENEFIT_DETAILS_MESSAGE)
+    }
+
+    fun printBenefitAmountTotal() {
+        println(BENEFIT_AMOUNT_TOTAL_MESSAGE)
+    }
+
+    fun printAfterDiscountTotal() {
+        println(AFTER_DISCOUNT_TOTAL_MESSAGE)
+    }
+
+    fun printEventBadge() {
+        println(DECEMBER_EVENT_BADGE)
+    }
+}
