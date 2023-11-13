ccc82c6ed5b530b90b5612c9ed1fa0eee8512735
3uomlkh <cw1205@duksung.ac.kr>
2023-11-14 02:09:59 +0900

    feat: 평일 할인과 주말 할인 적용 기능 구현

--- src/main/kotlin/christmas/domain/Event.kt
+++ src/main/kotlin/christmas/domain/Event.kt
@@ -21,16 +21,30 @@ fun evenStart(menu: List<Order>, date: Int, total: Int) {
 
 fun inRangeChristmas(menu: List<Order>, date: Int, total: Int) {
     // 크리스마스 디데이 할인과 그 외 할인 적용
-    christmasDiscount(date)
+    val christmas = christmasDiscount(date)
+
+    val original = EventResult(christmas = christmas)
+    var new = EventResult()
+
+    when (dayOfTheWeek(date)) {
+        Event.WEEKDAYS.discount -> new = original.copy(weekday = weekDaysDiscount(menu))
+        Event.WEEKEND.discount -> new = original.copy(weekend = weekEndDiscount(menu))
+        Event.SUNDAY.discount -> println("특별 할인")
+    }
 }
 
 fun afterChristmas(menu: List<Order>, date: Int, total: Int) {
     // 그 외 할인만 적용
+    val original = EventResult()
+    var new = EventResult()
+
     when (dayOfTheWeek(date)) {
-        Event.WEEKDAYS.discount -> println("평일")
-        Event.WEEKEND.discount -> println("주말")
-        Event.SUNDAY.discount -> println("일요일")
+        Event.WEEKDAYS.discount -> new = original.copy(weekday = weekDaysDiscount(menu))
+        Event.WEEKEND.discount -> new = original.copy(weekend = weekEndDiscount(menu))
+        Event.SUNDAY.discount -> println("특별 할인")
     }
+
+    println(new.toString())
 }
 
 fun christmasDdayCheck(date: Int): Boolean {
@@ -48,5 +62,34 @@ fun dayOfTheWeek(date: Int): String {
 }
 
 fun christmasDiscount(date: Int): Int {
-    return 900 + 100 * date
+    return 900 + Event.CHRISTMAS_D_DAY.price * date
+}
+
+fun weekDaysDiscount(order: List<Order>): Int {
+    var discountPrice = 0
+    for (index in order.indices) {
+        val menu = order[index].menu
+        val quantity = order[index].quantity.toInt()
+        if(menu.equals(Menu.CHOCOLATE_CAKE.dish)
+            || menu.equals(Menu.ICE_CREAM.dish)) {
+            discountPrice += Event.WEEKDAYS.price * quantity
+        }
+    }
+    return discountPrice
+}
+
+fun weekEndDiscount(order: List<Order>): Int {
+    var discountPrice = 0
+    for (index in order.indices) {
+        val menu = order[index].menu
+        val quantity = order[index].quantity.toInt()
+        if(menu == Menu.T_BONE.dish
+            || menu == Menu.BBQ.dish
+            || menu == Menu.SEAFOOD_PASTA.dish
+            || menu == Menu.CHRISTMAS_PASTA.dish
+        ) {
+            discountPrice += Event.WEEKDAYS.price * quantity
+        }
+    }
+    return discountPrice
 }
